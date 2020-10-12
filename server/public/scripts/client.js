d5d69707322de53c3837ee7b1d605538ac58df7e
Elliot Malcolm <talcumalc@gmail.com>
2020-10-11 19:20:07 -0500

    troubleshooting operator assignment

---
+++ server/public/scripts/client.js
@@ -0,0 +1,158 @@
+console.log( 'welcome to js ');
+
+$(document).ready( makeReady );
+$("#sumSubmitBtn").on( 'click', )
+$(".operator").on( 'click', determineOperator )
+
+input1 = 0;
+input2 = 0;
+operator = null;
+
+console.log( $(".operator").attr());
+
+// takes in button values and assigns operator
+// function determineOperator()
+// {
+//     $(".operator").empty();
+//     if ($(".operator").attr() == '+'
+//     ($("subtractBtn").on( 'click', determine );
+
+// }
+
+// function assignAddition();
+// {
+//     operator = '+';
+// }
+
+// function assignSubtraction();
+// {
+//     operator = '-'
+// }
+
+// function assignDivision();
+// {
+//     operator = '/'
+// }
+
+// function assignMultiplication();
+// {
+//     operator = '*'
+// }
+
+
+
+// function determineOperator()
+// {
+//     $(".operator").empty(); 
+//     if ($(".operator").attr() == '+'){
+//         operator = '+';
+//         console.log( 'operator = +');
+//     }
+
+
+// }
+
+// function assignAddition();
+// {
+//     operator = '+';
+// }
+
+// function assignSubtraction();
+// {
+//     operator = '-'
+// }
+
+// function assignDivision();
+// {
+//     operator = '/'
+// }
+
+// function assignMultiplication();
+// {
+//     operator = '*'
+// }
+
+
+
+
+
+// function submitEquation()
+// { 
+//     console.log('Input 1:', input1, 'Input 2:', input2, 'Operator:' operator);
+//     let input1 = $('#input1').attr();
+//     )
+  
+//       $.ajax
+//       ({
+//           method: 'POST',
+//           url: '/calculations',
+//           data: 
+//           {
+//             input1 : ,
+//             input2 : 
+//             operator : ,
+//           }
+//       }).then(function(response){
+//           console.log('response was', response);
+          
+          
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    //       // append to the table in the DOM
+    //       $("#guessTable").append(`
+    //       <tr>
+    //         <td>${counter}</td>
+    //         <td>${peterGuess}</td>
+    //         <td>${elliotGuess}</td>
+    //         <td>${nickGuess}</td>
+    //         <td>${mattGuess}</td>
+    //     </tr>
+    //       `);
+    //       $('.guessInput').val('');
+          
+    //   }).catch(function(error){
+    //       //notifying the user of an error in post request
+    //       alert(error);
+
+    // <label for="input1">First number:</label>
+    // <input class="inputField" type="number" id="firstInput" placeholder="First number">
+    // <button id="addBtn"> + </button>
+    // <button id="subtractBtn"> - </button>
+    // <button id="multBtn"> * </button>
+    // <button id="divBtn"> / </button>
+    // <label for="male">Second number:</label>
+    // <input class="inputField" type="number" id="secondInput" placeholder="Second number">
+    // <!-- submit guess button  -->
+    // <button id="sumBtn"> = </button>
+    // <!-- clear inputs button  -->
+    // <button id="clearBtn"> C </button>
